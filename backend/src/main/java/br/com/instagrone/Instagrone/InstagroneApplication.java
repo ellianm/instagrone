5018f94792a452f73a2efe897d3c9a6017996a26
Gabriel Emer <gabriel_emer@hotmail.com>
2021-02-09 22:02:39 -0300

    Criação do Backend.

---
+++ backend/src/main/java/br/com/instagrone/Instagrone/InstagroneApplication.java
@@ -0,0 +1,13 @@
+package br.com.instagrone.Instagrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagroneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/instagrone/Instagrone/InstagroneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.instagrone.Instagrone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
