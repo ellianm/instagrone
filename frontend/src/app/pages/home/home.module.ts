2e73f903e204a255b99cce5c0448b56fff05c614
ellianm <ellianmarcondes@hotmail.com>
2021-02-12 08:54:59 -0300

    new features

--- frontend/src/app/pages/home/home.module.ts
+++ frontend/src/app/pages/home/home.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { CoreModule } from 'src/app/core/core.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { StoriesComponent } from './stories/stories.component';
 
 @NgModule({
     imports:
         [CommonModule,
          BrowserModule,
+         AppRoutingModule,
          CoreModule],
-    declarations: [HomeComponent],
+    declarations: [HomeComponent, StoriesComponent],
     exports: [HomeComponent]
 })
 export class HomeModule { }
---
+++ frontend/src/app/pages/home/stories/stories.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stories',
+  templateUrl: './stories.component.html',
+  styleUrls: ['./stories.component.scss']
+})
+export class StoriesComponent implements OnInit {
+
+  stories = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.test();
+    console.log(this.stories);
+  }
+
+  test() {
+    this.stories.push({
+      userName: "pedrinho",
+      imgs: [
+        {
+          dateInc: new Date,
+          imgUrl: "https://cdn.pixabay.com/photo/2012/11/21/17/02/lion-66898_960_720.jpg",
+          seq: 1
+        }
+      ]
+    },{
+      userName: "joaozinho",
+      imgs: [
+        {
+          dateInc: new Date,
+          imgUrl: "https://cdn.pixabay.com/photo/2012/11/21/17/02/lion-66898_960_720.jpg",
+          seq: 1
+        }
+      ]
+    })
+  }
+
+}
